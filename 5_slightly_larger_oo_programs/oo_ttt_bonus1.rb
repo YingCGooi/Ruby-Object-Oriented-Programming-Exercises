12c40454c196482f32806cbfaec409fbcf357011
Ying Chyi Gooi <Ying Chyi Gooi>
2017-06-09 22:17:01 -0500

    updating TTT AI

--- 5_slightly_larger_oo_programs/oo_ttt_bonus1.rb
+++ 5_slightly_larger_oo_programs/oo_ttt_bonus1.rb
@@ -2,6 +2,11 @@ module Displayable
   CONSOLE_WIDTH = 80
 
   private
+
+  def display_welcome_message
+    bannerize "Welcome to Tic-Tac-Toe Expansion!"
+  end
+
   def bannerize(message, style = "=")
     puts style * CONSOLE_WIDTH
     center_print(message)
@@ -21,12 +26,39 @@ module Displayable
   end
 
   def alert(message)
-    puts "<!>...#{message}..."
+    puts "<!> #{message}..."
   end
 
   def display_player_created
     bannerize("#{self.class} player created!" \
-    " Name =>'#{self.name}' | Marker =>'#{self.marker}'", '-')
+    " Name =>'#{name}' | Marker =>'#{marker}'", '-')
+  end
+
+  def choice_or_default(choice, default_marker)
+    choice.empty? ? default_marker : choice.upcase
+  end
+
+  def count_down_to_game_start
+    alert "Game starting in...3"
+    # sleep 1
+    alert "...2"
+    # sleep 1
+    # alert "...1"
+    # sleep 1
+  end
+
+  def display_goodbye_message
+    bannerize "Thank you for playing Tic-Tac-Toe Expansion! Goodbye!"
+  end
+
+  def joinor(arr, delimiter=', ', word='or')
+    before_last_words = arr.take(arr.size - 1).join(delimiter)
+    case arr.size
+    when 0    then ' '
+    when 1    then arr.first.to_s
+    when 2    then arr.join(" #{word} ")
+    when 3..99 then before_last_words + " #{word} " + arr.last.to_s
+    end
   end
 end
 
@@ -37,9 +69,10 @@ class TTTGame
 
   def initialize
     display_welcome_message
-    choice = get_game_mode_choice
+    choice = prompt_game_mode_choice
     @players = initialize_players(choice)
     @board = initialize_board(choice)
+    count_down_to_game_start
   end
 
   def initialize_players(choice)
@@ -57,90 +90,174 @@ class TTTGame
     end
   end
 
-  def get_game_mode_choice
+  def prompt_game_mode_choice
     alert "Before we begin, let's set up the game"
     puts ""
     loop do
       prompt "Select game mode (leave blank for default mode 0):"
-      puts "  (0) Board: 3x3 | Players: Human, Computer"
-      puts "  (1) Board: 5x5 | Players: Human, Computer"
-      puts "  (2) Board: 5x5 | Players: Human, Computer, Computer"
-      puts "  (3) Board: 5x5 | Players: Human, Human, Computer"
+      puts "  (0) Board: 3x3 | In-a-row: 3 | Players: Human, Computer"
+      puts "  (1) Board: 5x5 | Matches: 4 | Players: Human, Computer"
+      puts "  (2) Board: 5x5 | Matches: 4 | Players: Human, Computer, Computer"
+      puts "  (3) Board: 5x5 | Matches: 4 | Players: Human, Human, Computer"
       choice = gets.chomp
-      return choice.to_i if (%W[#{''} 0 1 2 3]).include? choice
+      return choice.to_i if %W[#{''} 0 1 2 3].include? choice
       alert "Invalid input, please enter a number between (1-3)"
     end
   end
 
-  def display_welcome_message
-    bannerize "Welcome to Tic-Tac-Toe Expansion!"
-  end
-
   def clear_screen; system("clear") || system("cls"); end
 
-  def display_board
-    players.each {|x| print [x.class, x.name, x.marker]}
+  def clear_screen_and_display_board
+    clear_screen
+    players.each { |x| print [x.class, x.name, x.marker] }
     puts ""
     board.draw
     puts ""
   end
 
-  def players_move_until_board_full_or_win
+  def players_move
+    players.each do |player|
+      player.move(board)
+      clear_screen_and_display_board
+      break if board.full? || board.someone_won?
+    end
+  end
+
+  def calculate_scores(winner)
+    winner.score += 1
+  end
+
+  def display_winning_player(winner)
+    bannerize("#{winner.class} player #{winner.name} won this turn!", '.')
+  end
+
+  def winner
+    players.find { |player| player.marker == board.winning_marker }
+  end
+
+  def score_board
+    players.map do |player|
+      "#{player.name}'s score: #{player.score}"
+    end.join(' | ')
+  end
+
+  def calculate_scores_and_display_results
+    if board.someone_won?
+      calculate_scores(winner)
+      display_winning_player(winner)
+    else
+      alert "The board is full! It's a tie"
+    end
+
+    bannerize(score_board, '-')
+  end
+
+  def continue?
     loop do
-      players.each do |player|
-        player.move(board)
-        return if board.full || board.someone_won?
-      end
+      prompt "Hit Enter to continue, or 'x' to exit..."
+      answer = gets.chomp
+      break answer.empty? if ['', 'x'].include? answer.downcase
+      alert "Sorry, must be Enter or 'x'"
+    end
+  end
+
+  def player_reached_max_score?
+    !!final_winner
+  end
+
+  def final_winner
+    players.find { |player| player.score >= 5 }
+  end
+
+  def display_final_winner
+    if final_winner.is_a?(Human)
+      bannerize "Congratulations, #{final_winner.name}! You have won the game!"
+    else
+      bannerize "#{final_winner.class} player #{final_winner.name} won the game!"
     end
   end
 
   def play
     clear_screen
     loop do
-      display_board
-
+      clear_screen_and_display_board
       loop do
-        players_move_until_board_full_or_win
-        clear_screen_and_display_board
+        players_move
+        break if board.full? || board.someone_won?
       end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
+      clear_screen_and_display_board
+      calculate_scores_and_display_results
+      board.reset
+      break display_final_winner if player_reached_max_score?
+      break unless continue?
     end
     display_goodbye_message
   end
 end
 
+#===============================================================
+#==============================BOARD============================
+#===============================================================
 class Board
   INITIAL_MARKER = ' '
   attr_reader :size, :squares, :winning_lines
+
   def initialize(size)
     @size = size
-    @squares = [INITIAL_MARKER] * (size ** 2)
-    @win_lines = calculate_win_lines
+    reset
+    @winning_lines = calculate_winning_lines
+  end
+
+  def reset
+    @squares = [INITIAL_MARKER] * (size**2)
+  end
+
+  def draw_vertical_lines
+    puts "     |" * (size - 1)
+  end
+
+  def draw_num_vert_lines(idx)
+    ((-size + 1)..0).each do |i|
+      if squares[idx + i - 1] == INITIAL_MARKER
+        print "   #{(idx + i).to_s.rjust(2)}"
+      else
+        print "     "
+      end
+      i < 0 ? (print "|") : (print "\n")
+    end
+  end
+
+  def last_square?(idx)
+    idx >= (size**2) - 1
+  end
+
+  def draw_horizontal_line(idx)
+    return if last_square?(idx)
+    puts "-----+" * (size - 1) + "-----"
+    draw_vertical_lines
   end
 
   def draw
-    puts "     |" * (size - 1)
-    squares.map.with_index do |square, idx|
+    draw_vertical_lines
+    squares.each_with_index do |square, idx|
       if (idx + 1) % size != 0
         print "  #{square}  |"
       else
         puts "  #{square}"
-        puts "     |" * (size - 1)
-        if idx < (size ** 2) - 1
-          puts "-----+" * (size - 1) + "-----"
-          puts "     |" * (size - 1)
-        end
+        draw_num_vert_lines(idx + 1)
+        draw_horizontal_line(idx)
       end
     end
   end
 
-  def unmarked_sq_nums
+  def unmarked_sq_nums(offset = 1)
     squares.map.with_index do |sq, idx|
-      sq == INITIAL_MARKER ? (idx + 1) : nil
+      sq == INITIAL_MARKER ? (idx + offset) : nil
     end.compact
+  end
+
+  def unmarked_sq_idx
+    unmarked_sq_nums(0)
   end
 
   def []=(num, marker)
@@ -151,37 +268,78 @@ class Board
     unmarked_sq_nums.empty?
   end
 
-  def calculate_win_lines
-    sq_nums = (1..size ** 2).to_a
-    rows =
-      sq_nums.each_cons(size).map.with_index do |(*row), idx|
-        row if idx % size == 0
-      end.compact
+  def calculate_win_rows(arr)
+    arr.each_cons(size).map.with_index do |(*row), idx|
+      row if idx % size == 0
+    end.compact
+  end
 
-    cols =
-      rows.first.zip(*rows[1..-1])
+  def calculate_win_diagonals(rows)
+    [(0...rows.size).map { |i| rows[i][i] },
+     (0...rows.size).map { |i| rows[i][rows.size - 1 - i] }]
+  end
 
-    diagonals =
-      [
-        (0...rows.size).map { |i| rows[i][i] },
-        (0...rows.size).map { |i| rows[i][rows.size - 1 - i] }
-      ]
+  def calculate_win_cols(rows)
+    rows.first.zip(*rows[1..-1])
+  end
 
-    p rows + cols + diagonals
+  def calculate_winning_lines
+    sq_nums = (0...size**2).to_a
+    rows = calculate_win_rows(sq_nums)
+    cols = calculate_win_cols(rows)
+    diagonals = calculate_win_diagonals(rows)
+    rows + cols + diagonals
+  end
+
+  def winning_mark(squares, n)
+    # squares.uniq.size == 1 && !squares.include?(' ') if size == 3
+    print squares
+    squares.find { |sq| squares.count(sq) == n && sq != INITIAL_MARKER }
+  end
+
+  def someone_won?
+    !!winning_marker
+  end
+
+  def n_identical_markers
+    case size
+    when 3 then 3
+    when 5 then 4
+    when 7..9 then 5
+    end
+  end
+
+  def winning_marker
+    winning_lines.each do |line|
+      squares_check = @squares.values_at(*line)
+      winning_mark = winning_mark(squares_check, n_identical_markers)
+      return winning_mark unless winning_mark.nil?
+    end
+    nil
   end
 end
 
+#===============================================================
+#==============================PLAYER===========================
+#===============================================================
 class Player
   include Displayable
 
   @@created_markers = { human: [], computer: [] }
   @@names = ['R2D2', 'Hal', 'Sonny']
   @@marker_list = %w[X O V N]
+
   attr_reader :name, :marker
+  attr_accessor :score
+
+  def initialize
+    @score = 0
+  end
 end
 
 class Human < Player
   def initialize
+    super
     @name = prompt_name
     @marker = prompt_input_marker
     display_player_created
@@ -208,7 +366,6 @@ class Human < Player
 
   def prompt_name
     loop do
-      puts ""
       prompt "Human player#{second_or_nothing}, please enter your name:"
       n = gets
       return capitalize_words(n.chomp) unless invalid_name?(n.chomp)
@@ -240,24 +397,23 @@ class Human < Player
     end
   end
 
-  def choice_or_default(choice, default_marker)
-    choice.empty? ? default_marker : choice.upcase
-  end
-
   def move(board)
-    puts "Choose a square between #{board.unmarked_sq_nums.join(', ')}: "
+    p board.squares
+    p board.winning_lines
+    puts "#{name}, choose a square between #{joinor(board.unmarked_sq_nums)}: "
     square =
       loop do
         n = gets.chomp.to_i
         break n if board.unmarked_sq_nums.include?(n)
         alert "Sorry, that's not a valid choice"
       end
-    board[square] = self.marker
+    board[square - 1] = marker
   end
 end
 
 class Computer < Player
   def initialize
+    super
     @name = @@names.delete(@@names[0..2].sample)
     @marker = @@marker_list.shift
     # get the first remaining marker from list
@@ -266,14 +422,61 @@ class Computer < Player
   end
 
   def move(board)
-    choice = board.unmarked_sq_nums.sample
-    board[choice] = self.marker
+    square_idx =
+      offence_idx(board) ||
+      defence_idx(board) ||
+      center_idx(board)  ||
+      random_idx(board)
+    board[square_idx] = marker
+  end
+
+  def random_idx(board)
+    board.unmarked_sq_idx.sample
+  end
+
+  def center_idx(board)
+    center = board.size ** 2 / 2
+    center if board.squares[center] == Board::INITIAL_MARKER
+  end
+
+  def opportunity?(squares)
+    ([marker] * 2 + [Board::INITIAL_MARKER]).sort == squares.sort
+  end
+
+  def danger?(squares)
+    squares.select do |sq|
+      ![Board::INITIAL_MARKER, marker].include? sq
+    end.count == 2
+  end
+
+  def unmarked_sq_idx(line, squares)
+    line.find.with_index { |_, i| squares[i] == Board::INITIAL_MARKER }
+  end
+
+  def defence_idx(board)
+    square_at_offence_risk_idx(board, false)
+  end
+
+  def offence_idx(board)
+    square_at_offence_risk_idx(board)
+  end
+
+  def square_at_offence_risk_idx(board, offence = true)
+    board.winning_lines.each do |line|
+      squares = board.squares.values_at(*line)
+      at_risk = offence ? opportunity?(squares) : danger?(squares)
+      return unmarked_sq_idx(line, squares) if at_risk
+    end
+    nil
   end
 end
 
-board = Board.new(5)
-Human.new.move(board)
-Computer.new.move(board)
-board.draw
-# game = TTTGame.new
-# game.play
+# board = Board.new(3)
+# p board.winning_lines
+# Human.new.move(board)
+# Computer.new.move(board)
+# board.draw
+# Human.new.move(board)
+# board.draw
+game = TTTGame.new
+game.play
