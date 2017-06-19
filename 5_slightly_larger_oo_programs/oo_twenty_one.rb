53bce6dbe59ac0751f372d75c75f197056eb16c8
Ying Chyi Gooi <Ying Chyi Gooi>
2017-06-19 16:41:40 -0500

    updating oo 21 game

--- 5_slightly_larger_oo_programs/oo_twenty_one.rb
+++ 5_slightly_larger_oo_programs/oo_twenty_one.rb
@@ -16,18 +16,24 @@ module Displayable
   CONSOLE_WIDTH = 80
 
   def bannerize(message, style = '=')
-    horizon_bar(style)
+    p_horizon_bar(style)
     p_center(message)
-    horizon_bar(style)
+    p_horizon_bar(style)
   end
 
   def alert(message); puts yellow("<!> #{message}...") end
   def prompt(message); puts green(">> #{message}")     end
-  def horizon_bar(style = '-'); puts style * CONSOLE_WIDTH  end
+  def p_horizon_bar(style = '-'); puts style * CONSOLE_WIDTH end
+  def p_short_line; puts("-" * (CONSOLE_WIDTH / 2)).center(CONSOLE_WIDTH) end
+
+  def print_rjust(message)
+    offset = 9 * message.scan(/\[0m/).count
+    print(message.rjust(CONSOLE_WIDTH / 2 - 1 + offset))
+  end
 
   def p_center(message)
-    return puts message.center(CONSOLE_WIDTH + 17) if message.include?("[0m")
-    puts message.center(CONSOLE_WIDTH)
+    offset = 9 * message.scan(/\[0m/).count
+    puts message.center(CONSOLE_WIDTH + offset)
   end
 
   def prompt_enter
@@ -35,89 +41,140 @@ module Displayable
     gets
   end
 
-  def display_welcome_message
-    bannerize "Welcome to TwentyOne!"
-    alert "Dealing cards"
-    prompt_enter
-  end
+  def display_welcome_message; bannerize "Welcome to TwentyOne!" end
 
-  def clear_screen
-    system('clear') || system('cls')
-  end
+  def clear_screen; system('clear') || system('cls')  end
+
+  def capitalize_words(str); str.split.map(&:capitalize).join(' ') end
 end
 
 class TwentyOne
   include Displayable
 
   def initialize
+    display_welcome_message
     @deck = Deck.new
-    @players = [Human.new(@deck), Human.new(@deck), Computer.new(@deck)]
-    @dealer = @players.last
+    @players = initialize_players
+    @players.last.is_dealer? = true
   end
 
   def play
-    display_welcome_message
     players_turn
     show_result
   end
 
   private
 
-  def win_or_busted(player)
-    unless player.eql?(@dealer)
-      if player.total_value > 21
-        " - BUSTED!"
-      elsif player.total_value > @dealer.total_value || @dealer.bust?
-        " - WIN!"
-      else
-        " - LOST!"
-      end
-    else
-      if  @dealer.bust?
-        " - BUSTED!"
-      elsif @players.map { |player| player.total_value }.max == player.total_value
-        " - WIN!"
-      end
+  def prompt_num_players
+    loop do
+      prompt "Please choose the number of players (2-8)." \
+      "Leave empty for default (2):"
+      answer = gets.chomp
+      break answer.to_i if (2..8).cover? answer.to_i
+      break 2 if answer.empty?
+      alert "Invalid input. Try again"
     end
+  end
+
+  def prompt_dealer_type
+    loop do
+      prompt "(C)omputer or (h)uman as dealer?" \
+      "Leave empty for default (computer):"
+      answer = gets.chomp
+      break Human if answer.downcase == 'h'
+      break Computer if answer.downcase == 'c' || answer.empty?
+      alert "Invalid input. Try again"
+    end
+  end
+
+  def initialize_players
+    num_players = prompt_num_players
+    dealer_type = prompt_dealer_type
+
+    (1...num_players).map { Human.new(@deck) } + [dealer_type.new(@deck)]
+  end
+
+  def dealer_bust_or_win_mark
+    if @dealer.bust?
+      red(" - BUSTED!")
+    elsif @dealer.total_value > @players[0...-1].map(&:total_value).max
+      green(" - WIN!")
+    end
+  end
+
+  def player_bust_win_lost_or_tie_mark(player)
+    if player.bust?
+      red(" - BUSTED!")
+    elsif player.total_value > @dealer.total_value || @dealer.bust?
+      green(" - WIN!")
+    elsif player.total_value < @dealer.total_value
+      red(" - LOST!")
+    else
+      blue(" - TIE!")
+    end
+  end
+
+  def win_or_busted(player)
+    return dealer_bust_or_win_mark if player.eql?(@dealer)
+    player_bust_win_lost_or_tie_mark(player)
   end
 
   def show_result
     clear_screen
-    @players.each { |player| player.show_hand }
-    result =
-      @players.map do |player|
-        "#{player.name}: #{player.total_value} #{win_or_busted(player)}"
-      end
-    bannerize(result.join(' | '), '-')
+    @players.each_with_index do |player, idx|
+      p_horizon_bar if idx > 0
+      player.show_hand
+    end
+    p_horizon_bar("=")
+    p_center "Summary of scores"
+    p_short_line
+    @players.each do |player|
+      print_rjust "#{player.name}: #{player.total_value}"
+      puts " #{win_or_busted(player)}"
+    end
   end
 
   def other_players_reveal_one_card(current_player)
     @players.each do |player|
-      puts_first_card_only(player) if player != current_player
+      show_first_card_only(player) if player != current_player
     end
   end
 
-  def puts_first_card_only(player)
-    p_center "#{player.name} has #{player.hand.first} #{unknown_cards}"
+  def show_first_card_only(player)
+    print_rjust "#{dealer_or_not(player)}#{player.name} has:"
+    puts " #{player.hand.first}#{unknown_cards(player)}"
+    puts
   end
 
-  def unknown_cards
+  def unknown_cards(player)
+    " #{bg_gray(black('[?]'))}" * (player.hand.count - 1)
+  end
+
+  def dealer_or_not(player)
+    yellow("DEALER => ") if player.eql?(@dealer)
+  end
+
+  def display_next_player_name(idx)
+    "| Next player: #{@players[idx + 1].name}" if idx < @players.size - 1
+  end
+
+  def current_and_next(idx)
+    " #{@players[idx].name}'s turn #{display_next_player_name(idx)}"
   end
 
   def players_turn
-    @players.each do |current_player|
-
+    @players.each_with_index do |current_player, idx|
       loop do
         clear_screen
-        current_player.show_hand if current_player.is_a?(Human)
-        puts "-" * CONSOLE_WIDTH
+        p_center(current_and_next(idx))
+        puts
         other_players_reveal_one_card(current_player)
 
-        if !current_player.keep_hitting?(@deck) || current_player.bust?
-          break
-        end
-      end
+        p_horizon_bar
+        current_player.show_hand if current_player.is_a?(Human)
 
+        break if !current_player.keep_hitting?(@deck)
+      end
     end
   end
 end
@@ -168,7 +225,6 @@ class Card
   end
 
   def value; Deck::RANKS.index(rank) end
-
 end
 
 #============================================================================
@@ -180,6 +236,7 @@ class Player
   include Displayable
 
   attr_reader :hand, :name, :total_value
+  attr_accessor :is_dealer?
 
   def initialize(deck)
     @hand = []
@@ -211,29 +268,50 @@ class Player
   end
 
   def show_hand
-    p_center "#{name}, you have:"
-    hand.each { |card| puts card }
-    # p_center "#{hand.map(&:to_s).join(' ')}"
+    p_center "#{name}'s hand: #{hand.map(&:to_s).join(' ')}"
+    puts
     p_center "#{name}, you have total value of #{total_value} on hand"
   end
 end
 
 class Human < Player
+  @@names = []
+
   def initialize(deck)
     super
     @name = prompt_name
+    @@names << @name
+  end
+
+  def invalid_name?(n)
+    n.empty? || !n[/[a-z]/i] || same_existing_name?(n)
+  end
+
+  def same_existing_name?(n)
+    @@names.include?(capitalize_words(n))
+  end
+
+  def player_number
+    "Player ##{@@names.count + 1}"
   end
 
   def prompt_name
     loop do
-      prompt "Enter your name: "
-      answer = gets.chomp
-      break answer if !answer.empty?
-      alert "Sorry...must enter a name"
+      prompt "#{player_number}, please enter your name: "
+      n = gets.chomp
+      break capitalize_words(n) unless invalid_name?(n)
+      next alert "Cannot be the same name" if same_existing_name?(n)
+      alert "Sorry...name cannot be empty and must contain a letter"
     end
   end
 
   def keep_hitting?(deck)
+    if bust?
+      alert "You are busted"
+      prompt_enter
+      return false
+    end
+
     answer =
       loop do
         prompt "Enter to hit, 's' to stay..."
@@ -246,11 +324,9 @@ class Human < Player
 end
 
 class Computer < Player
-  ROBOTS = %w[R2D2 Hal Sonny]
-
   def initialize(deck)
     super
-    @name = ROBOTS.sample
+    @name = "A.I."
   end
 
   def keep_hitting?(deck)
